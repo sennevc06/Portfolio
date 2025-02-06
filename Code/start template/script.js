eff3018972eab7b0e2171439b37486b479421c1b
sennevc06 <sennevancrombrugge@gmail.com>
2025-02-06 16:00:51 +0100

    Delete Code directory

--- Code/start template/script.js
+++
@@ -1,7 +0,0 @@
-const menuBtn = document.querySelector('.menu-icon');
-const navMenu = document.querySelector('.nav-menu');
-
-
-menuBtn.addEventListener('click', () => {
-    navMenu.classList.toggle('show');
-});
